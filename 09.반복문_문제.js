c702c940282606d5d638141008e2b9cf0c315afa
liberoLab <libero221.lab@gmail.com>
2022-02-05 21:08:42 +0900

    문제 풀이 및 해답

--- 09.반복문_문제.js
+++ 09.반복문_문제.js
@@ -3,6 +3,13 @@ let sum1 = 0
 for (let i = 1; i <= 100; i++) {
     sum1 += i
 } console.log(sum1)
+/*
+let sum = 0
+for(let i=1;i<=100;i++){
+ sum+=i
+}
+console.log(sum)
+*/
 
 
 //문제 2. 1부터 100까지 홀수만 출력하자.
@@ -12,7 +19,11 @@ for (let i = 1; i <= 100; i++) {
         sum2 += i
     }
 } console.log(sum2)
-
+/*
+for(let i=1;i<=100;i+=2){
+  console.log(i)
+}
+*/
 
 
 //문제 3. 1부터 50까지 369결과를 프린트하자.
@@ -27,6 +38,43 @@ for (let i = 1; i <= 50; i++) {
     }
     console.log(result.length > 0 ? result : i)
 }
-
+/*
+for(let i=1;i<=50;i++){
+  let stringValue = i.toString()
+  let result = ""
+  for(let j=0;j<stringValue.length;j++){
+    if(stringValue[j] == "3" ||stringValue[j] == "6" ||stringValue[j] == "9" ){
+      result+="짝"
+    }
+  }
+  console.log(result.length>0?result:i)
+}
+*/
 
 //문제 4. 주어진 숫자가 소수이면 true 아니면 false를 출력하는 프로그램을 짜시오.
+let num = 40
+let TF = true
+
+if (num == 1) {
+    TF = false
+}
+for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+        TF = false
+    }
+}
+console.log(TF)
+/*
+let n =11
+let isPrime = true
+
+if (n ===1){// 1은 소수가 아님 
+    isPrime = false
+}
+for(let i=2;i<n;i++){
+  if(n % i == 0){
+    isPrime =  false 
+  }
+}
+console.log(isPrime)
+*/
